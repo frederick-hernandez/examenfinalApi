ae2dd8e710a25fc3023ec131fc89ee29ec728e4c
frederick-hernandez <53789782+frederick-hernandez@users.noreply.github.com>
2024-11-02 12:48:24 -0600

    subir

--- app/routers/proyectos.router.js
+++ app/routers/proyectos.router.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const rProyectos = require('../controllers/proyectos.controllers.js');
 
 router.post('/crear', rProyectos.create);
-router.get('/buscar/id', rProyectos.findById);
+router.get('/buscar/:id', rProyectos.findById);
 router.get('/todos', rProyectos.findAll);
-router.put('/put/id', rProyectos.update);
-router.delete('/delete/id',rProyectos.delete);
+router.put('/put/:id', rProyectos.update);
+router.delete('/delete/:id',rProyectos.delete);
 
 module.exports = router;
