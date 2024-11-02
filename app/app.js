18531122fee56bc06de34f4c8e10ab6c3dd22348
frederick-hernandez <53789782+frederick-hernandez@users.noreply.github.com>
2024-11-01 23:43:33 -0600

    cors

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = require('./routers/prueba.router.js');
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Welcome home')
 })
